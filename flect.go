50035f4fc9f87530de064e29fb0e7baf4b3113b5
Mark Bates <mark@markbates.com>
2018-07-28 14:48:44 +0100

    dasherize

--- flect.go
+++ flect.go
@@ -1,9 +1,41 @@
 package flect
 
+import (
+	"strings"
+	"unicode"
+)
+
 func isVowel(r rune) bool {
 	switch r {
 	case 'a', 'e', 'i', 'o', 'u':
 		return true
 	}
 	return false
+}
+
+var spaces = []rune{'_', ' ', ':', '-', '/'}
+
+func isSpace(c rune) bool {
+	for _, r := range spaces {
+		if r == c {
+			return true
+		}
+	}
+	return unicode.IsSpace(c)
+}
+
+func xappend(a []string, ss ...string) []string {
+	for _, s := range ss {
+		s = strings.TrimSpace(s)
+		for _, x := range spaces {
+			s = strings.Trim(s, string(x))
+		}
+		if _, ok := baseAcronyms[strings.ToUpper(s)]; ok {
+			s = strings.ToUpper(s)
+		}
+		if s != "" {
+			a = append(a, s)
+		}
+	}
+	return a
 }
--- ident.go
+++ ident.go
@@ -84,30 +84,3 @@ func toParts(s string) []string {
 
 	return parts
 }
-
-var spaces = []rune{'_', ' ', ':', '-', '/'}
-
-func isSpace(c rune) bool {
-	for _, r := range spaces {
-		if r == c {
-			return true
-		}
-	}
-	return unicode.IsSpace(c)
-}
-
-func xappend(a []string, ss ...string) []string {
-	for _, s := range ss {
-		s = strings.TrimSpace(s)
-		for _, x := range spaces {
-			s = strings.Trim(s, string(x))
-		}
-		if _, ok := baseAcronyms[strings.ToUpper(s)]; ok {
-			s = strings.ToUpper(s)
-		}
-		if s != "" {
-			a = append(a, s)
-		}
-	}
-	return a
-}
