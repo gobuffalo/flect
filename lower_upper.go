2949e73d74b0e81af7fa4c1286968017aa4ce43c
Mark Bates <mark@markbates.com>
2018-07-28 15:57:22 +0100

    removed the basic ToLower and ToUpper mappings

--- lower_upper.go
+++ lower_upper.go
@@ -3,17 +3,11 @@ package flect
 import "strings"
 
 // ToUpper is a convience wrapper for strings.ToUpper
-var ToUpper = strings.ToUpper
-
-// ToLower is a convience wrapper for strings.ToLower
-var ToLower = strings.ToLower
-
-// ToUpper is a convience wrapper for strings.ToUpper
 func (i Ident) ToUpper() Ident {
-	return New(ToUpper(i.Original))
+	return New(strings.ToUpper(i.Original))
 }
 
 // ToLower is a convience wrapper for strings.ToLower
 func (i Ident) ToLower() Ident {
-	return New(ToLower(i.Original))
+	return New(strings.ToLower(i.Original))
 }
