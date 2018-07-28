683d3451c719f7d3d6ee6cb9bd3cc5d16000d160
Mark Bates <mark@markbates.com>
2018-07-28 11:59:21 +0100

    Added a GroupName func

--- name_test.go
+++ name_test.go
@@ -21,6 +21,7 @@ func Test_Name(t *testing.T) {
 		{"This is `code` ok", "ThisIsCodeOK"},
 		{"foo_bar", "FooBar"},
 		{"admin/widget", "AdminWidget"},
+		{"admin/widgets", "AdminWidget"},
 		{"widget", "Widget"},
 		{"widgets", "Widget"},
 		{"status", "Status"},
@@ -35,6 +36,31 @@ func Test_Name(t *testing.T) {
 			r := require.New(st)
 			r.Equal(tt.exp, Name(tt.act))
 			r.Equal(tt.exp, Name(tt.exp))
+		})
+	}
+}
+
+func Test_GroupName(t *testing.T) {
+	table := []tt{
+		{"", ""},
+		{"Person", "People"},
+		{"foo_bar", "FooBars"},
+		{"admin/widget", "AdminWidgets"},
+		{"widget", "Widgets"},
+		{"widgets", "Widgets"},
+		{"greatPerson", "GreatPeople"},
+		{"great/person", "GreatPeople"},
+		{"status", "Statuses"},
+		{"Status", "Statuses"},
+		{"Statuses", "Statuses"},
+		{"statuses", "Statuses"},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.act, func(st *testing.T) {
+			r := require.New(st)
+			r.Equal(tt.exp, GroupName(tt.act))
+			r.Equal(tt.exp, GroupName(tt.exp))
 		})
 	}
 }
