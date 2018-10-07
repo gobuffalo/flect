ae7ed6bfe683063bab833454fa8a95a789139f3f
Matthew Clark <mclark4386@gmail.com>
2018-10-07 18:10:23 -0500

    Remove generic `ld` to `ldren` rule (#4)
    Closes #3

--- plural_rules.go
+++ plural_rules.go
@@ -165,7 +165,6 @@ func init() {
 	AddPlural("person", "people")
 	AddPlural("va", "vae")
 	AddPlural("leus", "li")
-	AddPlural("ld", "ldren")
 	AddPlural("oot", "eet")
 	AddPlural("oose", "eese")
 	AddPlural("box", "boxes")
