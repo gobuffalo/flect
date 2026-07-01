988d6f9b4b878a3d5623ae064b5e8e5ade35e813
Stanislas Michalak <stanislas.michalak@gmail.com>
2026-07-01 22:55:48 +0200

    chore: make root aliases a global barrel file

--- acronyms.go
+++
@@ -1,9 +0,0 @@
-package flect
-
-import "github.com/gobuffalo/flect/internal/core"
-
-// baseAcronyms and dictionary are the same references as in internal/core,
-// kept here so that package-level tests in this package can access them
-// without importing the internal package directly.
-var baseAcronyms = core.BaseAcronyms
-var dictionary = core.Dictionary
